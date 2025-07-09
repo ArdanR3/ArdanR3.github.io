90b7fae1478dcb4fea7b74eb2daf3a3a4d542562
ArdanR <ardanrizqi672@gmail.com>
2025-07-09 19:07:52 +0700

    icon

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker installed');
+self.addEventListener('install', function(e) {
+  console.log('Service Worker Installed');
 });
 
-self.addEventListener('fetch', (e) => {
+self.addEventListener('fetch', function(e) {
   e.respondWith(fetch(e.request));
 });
